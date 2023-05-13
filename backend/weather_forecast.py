ad7ca42a387bde3de70f96e3050164ada63d6b0a
so5613@student.uni-lj.si <so5613@student.uni-lj.si>
2023-05-14 00:10:48 +0200

    Visualization of forecast

--- backend/weather_forecast.py
+++ backend/weather_forecast.py
@@ -1,6 +1,8 @@
 import python_weather
 import os
 import asyncio
+import matplotlib.pyplot as plt
+from PIL import Image
 
 def classify_rainfall(mmph):
    #Slight rain: Less than 0.5 mm per hour. Moderate rain: Greater than 0.5 mm per hour, but less than 4.0 mm per hour. 
@@ -30,14 +32,72 @@ async def getweather(city):
         forecast = list(weather.forecasts)[0]
         for hourly in forecast.hourly:
             print(f' --> {hourly!r}')
+
+async def weather_visualization(city):
+    # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
+    async with python_weather.Client(unit=python_weather.METRIC) as client:
+        # fetch a weather forecast from specified location
+        weather = await client.get(city,locale="sl_SI")
+
+        # returns the current day's forecast temperature (int)
+        print(str(weather.current.temperature)+"°C" + " (feels like " + str(weather.current.feels_like)+"°C)")
+        print(classify_rainfall(weather.current.precipitation))
+        print(weather.current.kind)
+        print(weather.forecasts)
+
+        forecast_today = list(weather.forecasts)[0]
+        hourly_temperatures = []
+        hourly_precipitation = []
+        hourly_kind = []
+        for hourly in forecast_today.hourly:
+            hourly_temperatures.append(hourly.temperature)
+            hourly_precipitation.append(hourly.precipitation)
+            hourly_kind.append(hourly.kind)
+        
+
+        #visualize the current forecast with matplotlib
+        fig, ax1 = plt.subplots()
+        ax2 = ax1.twinx()
+
+
+        ax1.set_xlabel('Time')
+        ax1.set_ylabel('Temperatures')
+        ax2.set_ylabel('Precipitation')
+        ax1.plot(range(0,22,3),hourly_temperatures,color="red")
+        ax2.bar(range(0,22,3),hourly_precipitation,color="blue",width=2)
+        
+        #nicer formatting of time axis
+        ax1.set_xticks(range(0,22,3))
+        ax1.set_xticklabels(list(map(lambda x: str(x)+":00",list(range(0,22,3)))))
+
+        #axis limits
+        ax1.set_xlim([0,21])
+        ax1.set_ylim([min(min(hourly_temperatures),0), max(max(hourly_temperatures),25)])
+        ax2.set_ylim([min(min(hourly_precipitation),0), max(max(hourly_precipitation),10)])
+
+        #additional text description
+        rainfall = classify_rainfall(weather.current.precipitation)
+        tempertature = (str(weather.current.temperature)+"°C" + " (feels like " + str(weather.current.feels_like)+"°C)")
+        current_description = f"Current conditions:\n{rainfall}\n{tempertature}"
+       
+        ax1.text(0.5, 24, current_description, fontsize=14,
+        verticalalignment='top', bbox=dict(boxstyle='round', facecolor='blue', alpha=0.25))
+        plt.title(f"Forecast for {city}")
+        #plt.show()
+        plt.savefig("weather_forecast.jpg")
+
+        image = Image.open("weather_forecast.jpg")
+        return image
     
         
 
 
 if __name__ == '__main__':
-  # see https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
-  # for more details
   if os.name == 'nt':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
   
-  asyncio.run(getweather("Nanos"))
+  asyncio.run(weather_visualization("Nanos"))
+  
+
+
+
