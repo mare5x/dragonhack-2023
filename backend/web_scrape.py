f1b840efa6f518d6b6adf8a45fa0c3cc515eb040
naceee <47631585+naceee@users.noreply.github.com>
2023-05-13 23:41:51 +0200

    added web scraped images from hribi.net

--- backend/web_scrape.py
+++ backend/web_scrape.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import re
+from geopy.geocoders import Nominatim
 
 LINK_PREFIX = "https://vreme.arso.gov.si"
 
@@ -40,7 +42,6 @@ def create_webcam_json():
         # get link to the latest image from the webcam
         dict_key = f"webcam_{location_id}{direction}_data.json"
         camera = scraped_json.get(dict_key)
-        webcam_url = None
         if camera is not None and len(camera) > 0:
             last_shot = camera[-1]
             webcam_url = LINK_PREFIX + last_shot["path"]
@@ -56,6 +57,54 @@ def create_webcam_json():
     json.dump(webcam_json, open("webcam_info.json", "w"), indent=4, ensure_ascii=False)
 
 
+def scrape_one_webcam(link, location, image_id):
+    """ scrape one webcam from hribi.net and print the url of the image """
+    scraped_text = requests.get(link)
+    text = scraped_text.text
+
+    all_urls = re.findall('class="slikakamera".*src="(.*whatsupcams.*hribi.net)', text)
+    if len(all_urls) == 0:
+        return
+
+    url = all_urls[0]
+
+    coordinates = get_location(location)
+    webcam_json = {
+        "location": location,
+        "coordinates": coordinates,
+        "direction": "unknown",
+        "region": "unknown",
+        "webcam_url": url
+    }
+    image_id = f"HRIBI-{image_id}_"
+    with open("webcam_info.json", "r") as f:
+        webcam_info = json.load(f)
+    webcam_info[image_id] = webcam_json
+    json.dump(webcam_info, open("webcam_info.json", "w"), indent=4, ensure_ascii=False)
+
+    save_image(url, f"{image_id}")
+
+
+def get_location(location):
+    """ get latitude and longitude of a location using geopy """
+    geolocator = Nominatim(user_agent="myGeocoder")
+    coordinates = geolocator.geocode(f"{location}, Slovenia")
+    if coordinates is None:
+        return 0, 0
+    return coordinates.longitude, coordinates.latitude
+
+
+def hribi_net_webcams():
+    scraped_text = requests.get("https://www.hribi.net/spletne_kamere_v_gorah")
+    text = scraped_text.text
+
+    results = re.findall('"/spletna_kamera/(.{3,30})/([0-9]{2,5})">', text)
+
+    for location, image_id in results:
+        url = f"https://www.hribi.net/spletna_kamera/{location}/{image_id}"
+        scrape_one_webcam(url, location, image_id)
+
+
 def download_images():
     """ download images from webcams and save them to disk """
     webcam_info = json.load(open("webcam_info.json", "r"))
@@ -65,5 +114,6 @@ def download_images():
 
 
 if __name__ == '__main__':
-    create_webcam_json()
-    download_images()
+    # create_webcam_json()
+    # download_images()
+    hribi_net_webcams()
