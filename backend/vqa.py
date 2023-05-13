deca49d30176d7d6c92ad2ee906f1fd576289eca
naceee <47631585+naceee@users.noreply.github.com>
2023-05-13 17:30:27 +0200

    return best n

--- backend/vqa.py
+++ backend/vqa.py
@@ -1,28 +1,50 @@
 from transformers import ViltProcessor, ViltForQuestionAnswering
 import requests
 from PIL import Image
+import json
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import torch
+
+PROCESSOR = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+MODEL = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
 
 
-def vqa(image: Image, question: str) -> str:
-    pass
+def vqa(image: Image, question: str, n=5) -> list:
 
-# prepare image + question
-# url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-url = "https://meteo.arso.gov.si//uploads/probase/www/observ/webcam/LJUBL-ANA_BEZIGRAD_dir/siwc_20230513-1100_LJUBL-ANA_BEZIGRAD_nw.jpg"
+    # prepare inputs
+    encoding = PROCESSOR(image, question, return_tensors="pt")
+
+    # forward pass
+    outputs = MODEL(**encoding)
+    logits = torch.softmax(outputs.logits, dim=1)
+    n_best_idx = torch.topk(logits, n).indices[0].tolist()
+
+    n_best_responses = [(MODEL.config.id2label[idx], logits[0][idx].item()) for idx in n_best_idx]
+    return n_best_responses
 
 
-image = Image.open(requests.get(url, stream=True).raw)
-# text = "How many cats are there?"
-text = "What is the weather like?"
+def main():
+    webcam_data = json.load(open("webcam_info.json"))
 
-processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
-model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+    for webcam_id, webcam in webcam_data.items():
+        f_name = f"webcam_images/{webcam_id}.jpg"
+        image = Image.open(f_name)
+        text = "What is the weather like?"
 
-# prepare inputs
-encoding = processor(image, text, return_tensors="pt")
+        best_answers = vqa(image, text, n=3)
 
-# forward pass
-outputs = model(**encoding)
-logits = outputs.logits
-idx = logits.argmax(-1).item()
-print("Predicted answer:", model.config.id2label[idx])
+        title = f"In {webcam['location']} the weather is: " \
+                f"\n-{best_answers[0][0]} with p={round(best_answers[0][1], 3)}" \
+                f"\n-{best_answers[1][0]} with p={round(best_answers[1][1], 3)}" \
+                f"\n-{best_answers[2][0]} with p={round(best_answers[2][1], 3)}"
+
+        img = mpimg.imread(f_name)
+        plt.imshow(img)
+        plt.title(title)
+        plt.plot()
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
