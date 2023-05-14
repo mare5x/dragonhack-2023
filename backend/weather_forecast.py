bd7c3488247364cf87ec25c046867d29abcd0354
so5613@student.uni-lj.si <so5613@student.uni-lj.si>
2023-05-14 11:04:10 +0200

    Updated forecast to show future times

--- backend/weather_forecast.py
+++ backend/weather_forecast.py
@@ -1,4 +1,3 @@
-import python_weather
 import json
 import matplotlib.pyplot as plt
 from PIL import Image
@@ -38,23 +37,24 @@ def WMOcode(num):
 def weather_visualization(coordinates,city):
     
     (lat,lon) = coordinates
-
-    #current_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,precipitation,weathercode"
-    #current_url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m"
     forecast_url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,precipitation,weathercode"
     
     #report_current = requests.get(current_url)
     report_forecast = (requests.get(forecast_url)).json()
 
-    #weather forcast by hours for today
-    hourly_temperatures = report_forecast["hourly"]["temperature_2m"][0:25]
-    hourly_precipitation = report_forecast["hourly"]["precipitation"][0:25]
-    hourly_weathercode = report_forecast["hourly"]["weathercode"][0:25]
+    now = datetime.datetime.now()
+    time_interval = now.hour if now.minute < 30 else (now.hour + 1)%24
 
+    #weather forcast by hours for today
+    hourly_temperatures = report_forecast["hourly"]["temperature_2m"][time_interval:time_interval+25]
+    hourly_precipitation = report_forecast["hourly"]["precipitation"][time_interval:time_interval+25]
+    hourly_weathercode = report_forecast["hourly"]["weathercode"][time_interval:time_interval+25]
+    time_range = list(range(time_interval, time_interval+25,3))
 
     # visualize the current forecast with matplotlib
     fig, ax1 = plt.subplots()
     ax2 = ax1.twinx()
+    
 
     ax1.set_xlabel('Time')
     ax1.set_ylabel('Precipitation')
@@ -65,7 +65,7 @@ def weather_visualization(coordinates,city):
     ax2.plot(range(0, 25), hourly_temperatures, color="red")
     
     ax1.set_xticks(range(0, 25, 3))
-    ax1.set_xticklabels(list(map(lambda x: str(x%24)+":00", list(range(0, 25, 3)))))
+    ax1.set_xticklabels(list(map(lambda x: str(x%24)+":00", time_range)))
 
     ax1.set_xlim([0, 24])
     
@@ -73,9 +73,6 @@ def weather_visualization(coordinates,city):
     ax2.set_ylim([min(min(hourly_temperatures)-2, 0), max(max(hourly_temperatures)+2, 25)])
 
     #describe the current weather
-    now = datetime.datetime.now()
-    time_interval = now.hour if now.minute < 30 else (now.hour + 1)%24
-
     rainfall = classify_rainfall(hourly_precipitation[time_interval])
     temperature = str(hourly_temperatures[time_interval]) + " °C"
     description = WMOcode(hourly_weathercode[time_interval])
@@ -85,7 +82,7 @@ def weather_visualization(coordinates,city):
                bbox=dict(boxstyle='round', facecolor='blue', alpha=0.25))
     
     plt.title(f"Forecast for {city}")
-    plt.show()
+    
     plt.savefig("weather_forecast.jpg")
     image = Image.open("weather_forecast.jpg")
     return image
